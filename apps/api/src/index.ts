b1bbb86161529b4d931861c0b4ba9d0e60a01b8f
atybdot <180611249+atybdot@users.noreply.github.com>
2025-05-18 06:42:17 +0530

    fix:add db-routes into main application

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,18 @@
+import "dotenv/config";
 import { serve } from "@hono/node-server";
+import { env } from "env";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { jwt, sign } from "hono/jwt";
 import { logger } from "hono/logger";
+import prisma from "prisma";
+import type { User } from "prisma/generated";
+import { app as dbRouter } from "./routers/db/index";
+
 const app = new Hono();
+
 app.use(logger());
+
 app.use(
 	"/*",
 	cors({
@@ -11,9 +20,46 @@ app.use(
 		allowMethods: ["GET", "POST", "OPTIONS"],
 	}),
 );
+
 app.get("/", (c) => {
 	return c.text("OK");
 });
+
+app.post("/token", async (c) => {
+	const body: Pick<User, "email" | "password"> = await c.req.json();
+	console.log(body);
+
+	if (body.email === "" || body.password === "") {
+		return c.json({ message: "missing inputs", status: 402 });
+	}
+	try {
+		const user = await prisma.user.findUniqueOrThrow({
+			where: {
+				email: body.email,
+				password: body.password,
+			},
+		});
+		const token = await sign(
+			{ id: user.id, name: user.firstName },
+			env.JWT_SEC,
+		);
+		return c.json({
+			"auth-token": token,
+			message: "Please store it in some where safe",
+		});
+	} catch (error) {
+		return c.json({
+			message: "unable to find user\nprovide proper details",
+			status: 402,
+			error,
+		});
+	}
+});
+
+app.get("/grahak-connect", async (c) => {});
+
+app.route("/db", dbRouter);
+
 serve(
 	{
 		fetch: app.fetch,
